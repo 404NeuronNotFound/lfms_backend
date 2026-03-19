4ab763c8d3bf31decee68107a5512926f54d0658
Keybeen <keybeen.videoeditor@gmail.com>
2026-03-19 14:22:39 +0800

    putting down os

--- lfms_backend/settings.py
+++ lfms_backend/settings.py
@@ -13,12 +13,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
-import os
-
-if os.environ.get("DJANGO_ENV") == "production":
-    from .settings_production import *
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -176,3 +170,9 @@ CORS_ALLOW_HEADERS = [
     "origin",
     "x-requested-with",
 ]
+
+import os
+
+if os.environ.get("DJANGO_ENV") == "production":
+    from .settings_production import *
+load_dotenv()
